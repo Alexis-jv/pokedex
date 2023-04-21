09d49e4e3998d1dab7ea1063a3b1da0109b8ec80
Alexis <aleskuu@gmail.com>
2023-04-21 15:27:44 +0200

    Adding link beetween Root route and Pokemon route

--- src/components/PokedexPage/components/PokeCard/index.js
+++ src/components/PokedexPage/components/PokeCard/index.js
@@ -1,25 +1,28 @@
 import { useState } from "react";
 import "./style.css";
 import "../../../../color-types.css";
+import { Link } from "react-router-dom";
 
 export default function PokeCard({ pokemon, isPending }) {
   return (
-    <div className={"PokeCard " + pokemon.types[0].type.name}>
-      <div>
-        {isPending && <p>#00</p>}
-        {isPending && <h3>-------</h3>}
-        {!isPending && <p>#{pokemon.id}</p>}
-        {!isPending && <h3>{pokemon.name}</h3>}
+    <Link to={"/" + pokemon.id} className="PokeCard">
+      <div className={pokemon.types[0].type.name}>
+        <div>
+          {isPending && <p>#00</p>}
+          {isPending && <h3>-------</h3>}
+          {!isPending && <p>#{pokemon.id}</p>}
+          {!isPending && <h3>{pokemon.name}</h3>}
+        </div>
+        {isPending && (
+          <img src={require("./pokemon-ball-icon-9.jpg")} alt="Loading"></img>
+        )}
+        {!isPending && (
+          <img
+            src={pokemon.sprites.front_default}
+            alt={pokemon.name + " sprite"}
+          ></img>
+        )}
       </div>
-      {isPending && (
-        <img src={require("./pokemon-ball-icon-9.jpg")} alt="Loading"></img>
-      )}
-      {!isPending && (
-        <img
-          src={pokemon.sprites.front_default}
-          alt={pokemon.name + " sprite"}
-        ></img>
-      )}
-    </div>
+    </Link>
   );
 }
