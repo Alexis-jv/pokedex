1e1ab25f7cf74907633f227480dd6783b2aad4d1
Alexis <aleskuu@gmail.com>
2023-04-25 15:30:22 +0200

    Adding resizing hook that update rendered Pokemon

--- src/routes/root/root.jsx
+++ src/routes/root/root.jsx
@@ -1,12 +1,43 @@
 import "./style.css";
-import { useState } from "react";
+import { useState, useEffect, useLayoutEffect } from "react";
 import PageSelector from "../../components/PageSelector/index.js";
 import PokedexPage from "../../components/PokedexPage";
 
+function useWindowSize() {
+  const [size, setSize] = useState([0, 0]);
+  useLayoutEffect(() => {
+    function updateSize() {
+      setSize([window.innerWidth, window.innerHeight]);
+    }
+    window.addEventListener("resize", updateSize);
+    updateSize();
+    return () => window.removeEventListener("resize", updateSize);
+  }, []);
+  return size;
+}
+
 function Root() {
+  const [windowWidth, windowHeight] = useWindowSize();
   const [page, setPage] = useState(1);
   const [limit, setLimit] = useState(12);
   const [pokemonCount, setPokemonCount] = useState();
+
+  useEffect(() => {
+    setLimit(updatePageLimit());
+  }, [windowWidth, windowHeight]);
+
+  function updatePageLimit() {
+    let col = 4;
+    let row = 5;
+    if (window.innerWidth <= 1080) col = 3;
+    if (window.innerWidth <= 800) col = 2;
+    if (window.innerHeight <= 780) row = 4;
+    if (window.innerHeight <= 640) row = 3;
+    if (window.innerHeight <= 500) row = 2;
+    if (window.innerWidth <= 500 && window.innerHeight <= 800) row = 3;
+    if (window.innerWidth <= 500 && window.innerHeight <= 640) row = 2;
+    return row * col;
+  }
 
   function changePage(value) {
     if (value < 1) setPage(1);
